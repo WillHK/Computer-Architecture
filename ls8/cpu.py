566e6248bfded19a03b02be4ba0631654f80dd49
W Haeck <whaeck@Ws-Mac-mini.domain>
2020-02-26 15:50:22 -0800

    first 2 days of assignment

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,34 +2,38 @@
 
 import sys
 
+HLT = 1
+LDI = 130
+PRN = 71
+MUL = 162
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.filename = sys.argv[1]
 
     def load(self):
         """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        try:
+            with open(self.filename) as f:
+                for line in f:
+                    comment_split = line.strip().split('#')
+                    value = comment_split[0].strip()
+                    if value == '':
+                        continue
+                    num = int(value, 2)
+                    # print(num)
+                    self.ram[address] = num
+                    address += 1
+        except FileNotFoundError:
+            print("File Not Found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -60,6 +64,30 @@ class CPU:
 
         print()
 
+    def ram_read(self, address):
+        return self.ram[address]
+    
+    def ram_write(self, address, value):
+        self.ram[address] = value
+    
     def run(self):
         """Run the CPU."""
-        pass
+        while True:
+            command = self.ram[self.pc]
+
+            if command == HLT:
+                sys.exit(0)
+            elif command == LDI:
+                register = self.ram[self.pc + 1]
+                value = self.ram[self.pc + 2]
+                self.reg[register] = value
+                self.pc += 3
+            elif command == PRN:
+                register = self.ram[self.pc + 1]
+                print(int(self.reg[register]))
+                self.pc += 2
+            elif command == MUL:
+                reg0 = self.ram[self.pc + 1]
+                reg1 = self.ram[self.pc + 2]
+                self.reg[reg0] = self.reg[reg0] * self.reg[reg1]
+                self.pc += 3
